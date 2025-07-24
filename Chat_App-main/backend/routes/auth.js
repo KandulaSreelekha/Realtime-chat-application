68f678ef0cbaa6d03214792953a6a1cd1a4dd28d
KandulaSreelekha <162598017+KandulaSreelekha@users.noreply.github.com>
2025-07-24 15:03:15 +0530

    Delete Chat_App-main directory

--- Chat_App-main/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authControllers = require("../controllers/auth");
-const wrapAsync = require("../middlewares/wrapAsync");
-
-router.post("/signup", wrapAsync(authControllers.registerUser));
-router.post("/signin", wrapAsync(authControllers.loginUser));
-
-module.exports = router;
